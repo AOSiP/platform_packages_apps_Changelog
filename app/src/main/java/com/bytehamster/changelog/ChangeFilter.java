791772f6f60b364ea286f671cf51e27a648f39bf
Harsh Shandilya <msfjarvis@gmail.com>
2019-02-05 16:38:07 +0530

    Add method to retrieve common projects
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>
    Change-Id: Idb49c6e3bde8ffbf69afa3c532e5112cb037c13d

--- app/src/main/java/com/bytehamster/changelog/ChangeFilter.java
+++ app/src/main/java/com/bytehamster/changelog/ChangeFilter.java
@@ -57,8 +57,7 @@ class ChangeFilter {
         String watchedDevices = prefs.getString("watched_devices", "<?xml version=\"1.0\" encoding=\"UTF-8\"?><devicesList></devicesList>");
         try {
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-            DocumentBuilder db;
-            db = dbf.newDocumentBuilder();
+            DocumentBuilder db = dbf.newDocumentBuilder();
             InputSource is = new InputSource();
             is.setCharacterStream(new StringReader(watchedDevices));
             Document mWatchedDoc = db.parse(is);
@@ -66,8 +65,8 @@ class ChangeFilter {
 
             if (!mUseProjectsList.isEmpty()) mUseProjectsList.clear();
 
-            if (! displayAll) {
-                mUseProjectsList.add("android_vendor_du");
+            if (!displayAll) {
+                mUseProjectsList.add("platform_vendor_aosip");
                 NodeList gitList = mWatchedDoc.getDocumentElement().getElementsByTagName("git");
                 for (int i = 0; i < gitList.getLength(); i++) {
                     if (gitList.item(i).getNodeType() != Node.ELEMENT_NODE) continue;
